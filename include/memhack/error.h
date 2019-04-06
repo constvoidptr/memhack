3f1e569a14d5c08830aad7f53697f76a10562bfc
constvoidptr <throwaway6245@outlook.com>
2019-04-06 22:20:29 +0200

    Add header file documentation

--- include/memhack/error.h
+++ include/memhack/error.h
@@ -1,6 +1,14 @@
+/*
+ * Defines the library error type and all functions in relation with that.
+ */
+
 #ifndef MH_ERROR_H
 #define MH_ERROR_H
 
+/* 
+ * The library return type. MH_SUCCESS and MH_ERROR_GENERIC are guaranteed to
+ * be 0 and 1 the order of the others is not.
+ */
 enum mh_error {
 	MH_SUCCESS,
 	MH_ERROR_GENERIC,
@@ -9,6 +17,13 @@ enum mh_error {
 	MH_ERROR_PROCESS_NOT_FOUND
 };
 
+/*
+ * mh_resolve_error - Resolves error type into a string.
+ *
+ * @error: The type of error
+ *
+ * Returns: A constant error message string
+ */
 const char * mh_resolve_error(enum mh_error error);
 
 #endif
--- include/memhack/memhack.h
+++ include/memhack/memhack.h
@@ -1,8 +1,19 @@
+/*
+ * This file defines all library function calls. It is written in a
+ * system-agnostic way, so there are no system specific types except inside
+ * macros. Furthermore, all functions return an enum mh_error to provide a
+ * consistant API.
+ */
+
 #ifndef MH_MEMHACK_H
 #define MH_MEMHACK_H
 
 #include "error.h"
 
+/*
+ * Some systems need an extra extern specifier. Build Memhack with BUILD_MH
+ * defined to export function calls to dynamic link libraries.
+ */
 #ifndef DECLSPEC
 #	ifdef _WIN32
 #		ifdef BUILD_MH
@@ -15,12 +26,57 @@
 #	endif
 #endif
 
+// Opaque struct that abstracts the system specific implementation
 struct mh_process;
 
+/*
+ * mh_process_attach - Attach to your target process. This must be done before
+ * any other API call using mh_process.
+ *
+ * @process:     Double pointer to the mh_process struct
+ * @target_name: Name of the process
+ *
+ * Returns:      Enum of type mh_error
+ */
 DECLSPEC enum mh_error mh_process_attach(struct mh_process **process, const char *target_name);
+
+/*
+ * mh_process_detach - Detaches from your target process and frees memory. Every
+ * API call using a mh_process after detaching will result in undefined
+ * behaviour.
+ *
+ * @process: Pointer to the mh_process struct
+ *
+ * Returns:  Enum of type mh_error
+ */
 DECLSPEC enum mh_error mh_process_detach(struct mh_process *process);
 
+/*
+ * mh_memory_read - Copies memory out of the virtual address space of the target
+ * process into your process.
+ *
+ * @process: Pointer to the mh_process struct
+ * @src:     Source location from where the memory is beeing copied
+ * @dst:     Destination of the buffer where the memory is written to
+ * @size:    Amount of bytes you want to copy. This SHOULD NOT exceed the size
+ *           your buffer - would result in an overflow.
+ *
+ * Returns:  Enum of type mh_error
+ */
 DECLSPEC enum mh_error mh_memory_read(const struct mh_process *process, void *src, void *dst, unsigned int size);
+
+/*
+ * mh_memory_read - Copies memory from your process into the targets memory,
+ * considering the virtual addess space.
+ *
+ * @process: Pointer to the mh_process struct
+ * @src:     Source location from where the memory is beeing copied
+ * @dst:     Destination of where the memory should be copied to
+ * @size:    Amount of bytes you want to copy. This SHOULD NOT exceed the size
+ *           of the targets process buffer - would result in an overflow.
+ *
+ * Returns: Enum of type mh_error
+ */
 DECLSPEC enum mh_error mh_memory_write(const struct mh_process *process, void *src, void *dst, unsigned int size);
 
 #endif
